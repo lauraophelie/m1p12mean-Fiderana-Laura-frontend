01e61803cefd0c9eaa0f6a7708382bcf0eb2a178
lauraophelie <lauraophelie1@gmail.com>
2025-03-18 02:52:27 +0300

    liste service

--- src/app/pages/back-office/services-liste/services-liste.component.ts
+++ src/app/pages/back-office/services-liste/services-liste.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild, ViewEncapsulation, viewChild } from '@angular/core';
 import { MaterialModule } from '../../../material.module';
 import { ServiceGarage, ServicesGarageService } from '../../../services/services-garage/services-garage.service';
 import { MatPaginator } from '@angular/material/paginator';
 import { Router } from '@angular/router';
 import { TablerIconsModule } from 'angular-tabler-icons';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-services-liste',
@@ -18,7 +19,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
   styleUrl: './services-liste.component.scss',
   encapsulation: ViewEncapsulation.None,
 })
-export class ServicesListeComponent implements OnInit {
+export class ServicesListeComponent implements OnInit, AfterViewInit {
   services: ServiceGarage[] = [];
   currentPage = 1;
   itemsPerPage = 10;
@@ -26,17 +27,37 @@ export class ServicesListeComponent implements OnInit {
 
   displayedColumns: string[] = ['nomService', 'descriptionService', 'actions'];
   detailsIcon = "solar:minimalistic-magnifer-zoom-in-outline"
+  dataSource = new MatTableDataSource<ServiceGarage>();
 
-  constructor(private readonly serviceGarageService: ServicesGarageService, private readonly router: Router) {}
+  constructor(
+    private readonly serviceGarageService: ServicesGarageService,
+    private readonly router: Router
+  ) {}
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatTable) table!: MatTable<ServiceGarage>;
 
   ngOnInit(): void {
     this.loadServices();
   };
 
+  ngAfterViewInit(): void {
+    // this.paginator.page.subscribe(() => {
+    //   this.currentPage = this.paginator.pageIndex + 1;
+    //   this.itemsPerPage = this.paginator.pageSize;
+    //   this.loadServices();
+    // });
+    this.dataSource.paginator = this.paginator;
+  }
+
   loadServices(): void {
     this.serviceGarageService.getServicesPages(this.currentPage, this.itemsPerPage).subscribe((response: any) => {
       this.services = response.data;
       this.totalItems = response.totalItems;
+      this.dataSource.data = this.services;
+
+      this.paginator.length = this.totalItems;
+      this.table.renderRows();
     });
   };
 
@@ -47,16 +68,4 @@ export class ServicesListeComponent implements OnInit {
   goToDetails(serviceId: string): void {
     this.router.navigate([`/back/service/liste/${serviceId}`]);
   }
-
-  nextPage(): void {
-    if(this.currentPage < Math.ceil(this.totalItems / this.itemsPerPage)) {
-      this.currentPage++;
-      this.loadServices();
-    }
-  };
-
-  previousPage(): void {
-    this.currentPage--;
-    this.loadServices();
-  };
 }
