b47bebd1b5f84f5a69b23712411ac15053da56ef
lauraophelie <lauraophelie1@gmail.com>
2025-03-18 05:16:05 +0300

    création composant liste prestation

---
+++ src/app/pages/back-office/prestations-liste/prestations-liste.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prestations-liste',
+  standalone: true,
+  imports: [],
+  templateUrl: './prestations-liste.component.html',
+  styleUrl: './prestations-liste.component.scss'
+})
+export class PrestationsListeComponent {
+
+}
