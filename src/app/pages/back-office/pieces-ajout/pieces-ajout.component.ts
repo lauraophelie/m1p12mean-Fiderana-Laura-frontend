f437f03b931614bc8a15a5a0dd15b592bc5b3527
lauraophelie <lauraophelie1@gmail.com>
2025-03-24 20:15:33 +0300

    formulaire d'ajout de pièces

--- src/app/pages/back-office/pieces-ajout/pieces-ajout.component.ts
+++ src/app/pages/back-office/pieces-ajout/pieces-ajout.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { PiecesAddComponent } from '../../../components/pieces-add/pieces-add.component';
 
 @Component({
   selector: 'app-pieces-ajout',
   standalone: true,
-  imports: [],
+  imports: [
+    PiecesAddComponent
+  ],
   templateUrl: './pieces-ajout.component.html',
   styleUrl: './pieces-ajout.component.scss'
 })
