9f3fedc0e84e3b5bef02b371678f8f6e6e2a1ce6
lauraophelie <lauraophelie1@gmail.com>
2025-03-29 04:18:53 +0300

    header client

--- src/app/layouts/front-office/sidebar/sidebar-front-data.ts
+++ src/app/layouts/front-office/sidebar/sidebar-front-data.ts
@@ -1,5 +1,27 @@
 import { NavItem } from "../../full/sidebar/nav-item/nav-item";
 
 export const navItems: NavItem[] = [
-
+  {
+    navCap: 'Home'
+  },
+  {
+    displayName: 'Accueil',
+    iconName: 'solar:home-outline'
+  },
+  {
+    displayName: 'Nos services',
+    iconName: 'solar:notebook-bold'
+  },
+  {
+    navCap: 'Vous',
+    divider: true
+  },
+  {
+    displayName: 'Vos véhicules',
+    iconName: 'solar:wheel-bold-duotone'
+  },
+  {
+    displayName: 'Vos rendez-vous',
+    iconName: 'solar:clipboard-list-linear'
+  }
 ];
