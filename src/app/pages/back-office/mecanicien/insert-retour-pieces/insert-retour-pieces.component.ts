e2cc130d60df2fd9beff51d22481643f1d529893
lauraophelie <lauraophelie1@gmail.com>
2025-04-01 03:05:50 +0300

    retours de pièces par mécanicien

--- src/app/pages/back-office/mecanicien/insert-retour-pieces/insert-retour-pieces.component.ts
+++ src/app/pages/back-office/mecanicien/insert-retour-pieces/insert-retour-pieces.component.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RetourPiece, RetourPiecesService } from '../../../../services/retour-pieces/retour-pieces.service';
+import { variableTest } from '../../../../../variables-test/variable';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from '../../../../material.module';
 
 @Component({
   selector: 'app-insert-retour-pieces',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MaterialModule
+  ],
   templateUrl: './insert-retour-pieces.component.html',
   styleUrl: './insert-retour-pieces.component.scss'
 })
-export class InsertRetourPiecesComponent {
+export class InsertRetourPiecesComponent implements OnInit {
+  idMecanicien: string = `${variableTest.mecanicien}`;
+  newRetourPiece: RetourPiece = {
+    dateRetour: '',
+    pieceId: '',
+    quantiteRetour: 0,
+    motifRetour: '',
+    mecanicienId: this.idMecanicien
+  };
+  listePieces: any[] = [];
 
+  constructor(
+    private readonly retourPiecesServices: RetourPiecesService
+  ) {}
+
+  ngOnInit(): void {
+    this.loadListePiece();
+  }
+
+  addRetourPiece(): void {
+    if(this.checkDataRetourPiece()) {
+      this.retourPiecesServices.insertRetourPiece(this.newRetourPiece).subscribe(() => {
+        this.newRetourPiece = {
+          dateRetour: '',
+          pieceId: '',
+          quantiteRetour: 0,
+          motifRetour: '',
+          mecanicienId: this.idMecanicien
+        }
+      })
+    }
+  }
+
+  checkDataRetourPiece(): boolean {
+    if(this.newRetourPiece.pieceId && this.newRetourPiece.quantiteRetour && this.newRetourPiece.motifRetour && this.newRetourPiece.mecanicienId) {
+      return true;
+    }
+    return false;
+  }
+
+  loadListePiece(): void {
+
+  }
 }
--- src/app/pages/back-office/mecanicien/retour-pieces/retour-pieces.component.ts
+++ src/app/pages/back-office/mecanicien/retour-pieces/retour-pieces.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { variableTest } from '../../../../../variables-test/variable';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
+import { RetourPiecesService } from '../../../../services/retour-pieces/retour-pieces.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../../../../material.module';
+import { BadgeStatusComponent } from '../../../../components/badge-status/badge-status.component';
 
 @Component({
   selector: 'app-retour-pieces',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    BadgeStatusComponent
+  ],
   templateUrl: './retour-pieces.component.html',
   styleUrl: './retour-pieces.component.scss'
 })
-export class RetourPiecesComponent {
+export class RetourPiecesComponent implements OnInit, AfterViewInit {
+  mecanicienId: string = `${variableTest.mecanicien}`;
+  listeRetours: any[] = [];
+  currentPage = 1;
+  itemsPerPage = 10;
+  totalItems = 0;
 
+  displayedColumns: string[] = ["dateRetour", "pieceId", "quantiteRetour", "motifRetour", "status", "actions"];
+  dataSource = new MatTableDataSource<any>();
+
+  constructor(
+    private readonly retourPiecesService: RetourPiecesService
+  ) {}
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatTable) table!: MatTable<any>;
+
+  ngOnInit(): void {
+    this.loadRetourPieces();
+  }
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  loadRetourPieces(): void {
+    this.retourPiecesService.getRetoursPiecesMecanicien(this.mecanicienId, this.currentPage, this.itemsPerPage).subscribe((response: any) => {
+      this.listeRetours = response.data;
+      this.totalItems = response.totalItems;
+      this.dataSource.data = this.listeRetours;
+
+      this.paginator.length = this.totalItems;
+      this.table.renderRows();
+    });
+  }
 }
