a6da2cefc66078458f3fc869b422c0b9dbd72b09
lauraophelie <lauraophelie1@gmail.com>
2025-04-03 03:03:39 +0300

    page d'accueil client

--- src/app/pages/front-office/accueil-client/intro-client/intro-client.component.ts
+++ src/app/pages/front-office/accueil-client/intro-client/intro-client.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../../../material.module';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-intro-client',
   standalone: true,
-  imports: [],
+  imports: [
+    MaterialModule
+  ],
   templateUrl: './intro-client.component.html',
   styleUrl: './intro-client.component.scss'
 })
 export class IntroClientComponent {
+  constructor(
+    private readonly router: Router
+  ) {}
 
+  goToAddPage(): void {
+    this.router.navigate(['/front/rdv/add']);
+  }
 }
